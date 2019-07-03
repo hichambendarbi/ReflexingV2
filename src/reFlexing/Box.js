9e0a82f357088e714591e4f1196fadadc2ec6631
hichambendarbi <49118568+hichambendarbi@users.noreply.github.com>
2019-07-03 22:42:12 +0100

    v3

--- src/reFlexing/Box.js
+++ src/reFlexing/Box.js
@@ -2,7 +2,16 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledElement = styled.div`
-  flex: 1 100%;
+  flex: 1;
+  background-color: red;
+  width: ${props => props.width};
+  width: ${props => props.width};
+  grow: ${props => props.grow};
+  shrink: ${props => props.shrink};
+  basis: ${props => props.basis};
+  order: ${props => props.order};
+  self: ${props => props.self};
+
   ${props => props.css}
 `;
 
@@ -14,5 +23,19 @@ export const Box = ({
   basis,
   order,
   flex,
+  width,
   self
-}) => <StyledElement css={css}>{children}</StyledElement>;
+}) => (
+  <StyledElement
+    css={css}
+    grow={grow}
+    shrink={shrink}
+    basis={basis}
+    order={order}
+    flex={flex}
+    self={self}
+    width={width}
+  >
+    {children}
+  </StyledElement>
+);
